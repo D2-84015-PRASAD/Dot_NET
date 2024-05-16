049781a06ff4559cf1aad31fa9bd80c0e38b01a8
D2_Prasad_84015 <prasadnikam873@gmail.com>
2024-05-16 19:08:03 +0530

    Que4 added

---
+++ Assignment_4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Que4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] arr = null;
+
+
+            Student student = new Student();
+            student.accept();
+            student.display();
+
+        }
+
+        
+
+        }
+    }
+
+
