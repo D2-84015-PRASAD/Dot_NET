86c248cff96d7141a35e2b4a6de56e93a645f26f
D2_Prasad_84015 <prasadnikam873@gmail.com>
2024-05-24 16:00:33 +0530

    Que 10 added

---
+++ Assignment_10/Program.cs
@@ -0,0 +1,56 @@
+﻿using Question_8;
+
+namespace Question_10
+{
+    public class SuperVisor:Employee
+    {
+        private int subbordinates;
+
+        public int MySubbordination
+        {
+            get { return subbordinates; }
+            set { subbordinates = value; }
+        }
+
+        public SuperVisor() { 
+        this.subbordinates = 0;
+        }
+
+        public SuperVisor(int subbordinates)
+        {
+            this.subbordinates = subbordinates;
+        }
+
+        public void Accept()
+        {
+            base.Accept();
+            base.MyDestination = "Supervisor";
+            Console.WriteLine("Enter Subbordinates : ");
+            this.subbordinates=Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void Display()
+        {
+            base.Print();
+            Console.WriteLine("Subbordinates : " + this.subbordinates);
+        }
+
+        public string ToString()
+        {
+            string st=base.ToString();
+            return "\n" + st + " Subbordinates : " + this.subbordinates;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperVisor superVisor = new SuperVisor();
+            superVisor.Accept();
+            superVisor.Display();
+            superVisor.ToString();
+        }
+    }
+}
