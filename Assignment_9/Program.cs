46b7348182680fe948a015c9a83cbff28e4d03df
D2_Prasad_84015 <prasadnikam873@gmail.com>
2024-05-24 15:58:39 +0530

    Que 8 added

---
+++ Assignment_9/Program.cs
@@ -0,0 +1,59 @@
+﻿using Question_8;
+using System.Data;
+
+namespace Question_9
+{
+    public class Manager:Employee
+
+    {
+        private double bonus;
+
+        public Manager()
+        {
+            base.MyDestination = "Manager";
+            this.bonus = 0;
+        }
+
+        public Manager(double bonus)
+        {
+            this.bonus = bonus;
+        }
+        public double MyBonus
+        {
+            get { return bonus; }
+            set { bonus = value; }
+        }
+
+        public void Accept()
+        {
+            base.Accept();
+            base.MyDestination = "Manager";
+            Console.WriteLine("Enter bonus : ");
+            this.bonus=Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine("Bonus : " + this.bonus);
+
+        }
+
+        public string ToString()
+        {
+            string st=base.ToString();
+            return st+" Bonus : "+this.bonus;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            manager.Accept();
+            manager.Print();
+
+           
+        }
+    }
+}
